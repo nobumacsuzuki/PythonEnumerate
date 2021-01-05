c515bba535b5b2524bb7ea53dd90a6dd653b741b
nobumacsuzuki <nobumac.suzuki@gmail.com>
2021-01-04 22:22:05 -0800

    variable name change

--- TestEnumerate.py
+++ TestEnumerate.py
@@ -2,10 +2,10 @@ import numpy as np
 
 def main():
     arrayTest = np.array([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], dtype=float)
-    for (index, element) in enumerate(arrayTest):
-        print(index, element)
-    for (index, element) in enumerate(arrayTest[1:10:2]):
-        print(index, element)
+    for (count, element) in enumerate(arrayTest):
+        print(count, element)
+    for (count, element) in enumerate(arrayTest[1:10:2]):
+        print(count, element)
 
 if __name__ == "__main__":
     main()
